ac29997410233dd5dafe549010ef24b1c7ae2296
Victor Kai <victorkai1516@gmail.com>
2017-10-22 21:08:47 -0700

    updated formatting

--- src/main.cpp
+++ src/main.cpp
@@ -44,10 +44,10 @@ string hasData(string s) {
 
 // Generate new path points from JMT
 void generateNewPoints(	Map map, 
-												vector<double> jmt_s, 
-												vector<double> jmt_d, 
-												vector<double> &path_x, 
-												vector<double> &path_y) {
+                        vector<double> jmt_s, 
+                        vector<double> jmt_d, 
+                        vector<double> &path_x, 
+                        vector<double> &path_y) {
 	for(double t = 0; t <= HORIZON; t += TIME_INCR) {
 		double s = 0;
 		double d = 0;
@@ -133,110 +133,109 @@ int main() {
         
         if (event == "telemetry") {
           // j[1] is the data JSON object
-					double car_x = j[1]["x"];
-					double car_y = j[1]["y"];
-					double car_s = j[1]["s"];
-					double car_d = j[1]["d"];
-					double car_yaw = j[1]["yaw"];
-					double car_speed = j[1]["speed"];
+	        double car_x = j[1]["x"];
+	        double car_y = j[1]["y"];
+	        double car_s = j[1]["s"];
+	        double car_d = j[1]["d"];
+	        double car_yaw = j[1]["yaw"];
+	        double car_speed = j[1]["speed"];
 
-					self_vehicle.x = car_x;
-					self_vehicle.y = car_y;
-					self_vehicle.s = car_s;
-					self_vehicle.d = car_d;
-					self_vehicle.speed = car_speed;
+	        self_vehicle.x = car_x;
+	        self_vehicle.y = car_y;
+	        self_vehicle.s = car_s;
+	        self_vehicle.d = car_d;
+	        self_vehicle.speed = car_speed;
 
-					// Previous path data given to the Planner
-					auto previous_path_x = j[1]["previous_path_x"];
-					auto previous_path_y = j[1]["previous_path_y"];
-					// Previous path's end s and d values 
-					double end_path_s = j[1]["end_path_s"];
-					double end_path_d = j[1]["end_path_d"];
+	        // Previous path data given to the Planner
+	        auto previous_path_x = j[1]["previous_path_x"];
+	        auto previous_path_y = j[1]["previous_path_y"];
+	        // Previous path's end s and d values 
+	        double end_path_s = j[1]["end_path_s"];
+	        double end_path_d = j[1]["end_path_d"];
 
-					// Sensor Fusion Data, a list of all other cars on the same side of the road.
-					auto sensor_fusion = j[1]["sensor_fusion"];
-					vector<Vehicle> vehicles;
-					for(int i = 0; i < sensor_fusion.size(); i++) {
-						double x = sensor_fusion[i][1];
-						double y = sensor_fusion[i][2];
-						double vx = sensor_fusion[i][3];
-						double vy = sensor_fusion[i][4];
-						double s = sensor_fusion[i][5];
-						double d = sensor_fusion[i][6];
-						double v = sqrt(vx*vx + vy*vy);
+	        // Sensor Fusion Data, a list of all other cars on the same side of the road.
+	        auto sensor_fusion = j[1]["sensor_fusion"];
+	        vector<Vehicle> vehicles;
+	        for(int i = 0; i < sensor_fusion.size(); i++) {
+		        double x = sensor_fusion[i][1];
+		        double y = sensor_fusion[i][2];
+		        double vx = sensor_fusion[i][3];
+		        double vy = sensor_fusion[i][4];
+		        double s = sensor_fusion[i][5];
+		        double d = sensor_fusion[i][6];
+		        double v = sqrt(vx*vx + vy*vy);
 
-						Vehicle vehicle(x,y,s,d,v);
-						vehicles.push_back(vehicle);
-					}
-					road.setRoad(vehicles);
+		        Vehicle vehicle(x,y,s,d,v);
+		        vehicles.push_back(vehicle);
+	        }
+	        road.setRoad(vehicles);
 
 
-					vector<double> next_x_vals = previous_path_x;
-					vector<double> next_y_vals = previous_path_y;
-					vector<double> start_s = self_vehicle.prev_s;
-					vector<double> start_d = self_vehicle.prev_d;
+	        vector<double> next_x_vals = previous_path_x;
+	        vector<double> next_y_vals = previous_path_y;
+	        vector<double> start_s = self_vehicle.prev_s;
+	        vector<double> start_d = self_vehicle.prev_d;
 
-					double start_speed = self_vehicle.speed;
-					double target_speed = MAX_SPEED / 2;
-					double target_d = get_lane_d(self_vehicle.get_lane());
-					
-					bool generate_new_points = false;
-					if(previous_path_x.size() == 0) {
-						start_s = {self_vehicle.s, self_vehicle.speed, 0};
-						start_d = {get_lane_d(self_vehicle.get_lane()), 0, 0};
-						self_vehicle.setPrev(start_s, start_d);
+	        double start_speed = self_vehicle.speed;
+	        double target_speed = MAX_SPEED / 2;
+	        double target_d = get_lane_d(self_vehicle.get_lane());
+	
+	        bool generate_new_points = false;
+	        if(previous_path_x.size() == 0) {
+		        start_s = {self_vehicle.s, self_vehicle.speed, 0};
+		        start_d = {get_lane_d(self_vehicle.get_lane()), 0, 0};
+		        self_vehicle.setPrev(start_s, start_d);
 
-						generate_new_points = true;
-					}
-					else if(previous_path_x.size() < 20) {
-						start_speed = self_vehicle.prev_s[1];
-						target_speed = min(start_speed + 2, MAX_SPEED);
+		        generate_new_points = true;
+	        }
+	        else if(previous_path_x.size() < 20) {
+		        start_speed = self_vehicle.prev_s[1];
+		        target_speed = min(start_speed + 2, MAX_SPEED);
 
-						State next_state = fsm.transitionState(self_vehicle, road);
-						if(next_state == State::KEEP_LANE) {
-							vector<double> distances = road.findClosestDistanceForwardAndBehind(self_vehicle, self_vehicle.get_lane());
-							double distanceForward = distances[1];
-							if(distanceForward <= 30) {
-								target_speed = start_speed * 0.8;
-							}
-						}
-						else if(next_state == State::CHANGE_LEFT) {
-							target_d = get_lane_d(self_vehicle.get_lane() - 1);
-						}
-						else if(next_state == State::CHANGE_RIGHT) {
-							target_d = get_lane_d(self_vehicle.get_lane() + 1);
-						}
-						// right lane adjustment
-						if(target_d >= 10.0) {
-							target_d -= 0.2;
-						}
+		        State next_state = fsm.transitionState(self_vehicle, road);
+		        if(next_state == State::KEEP_LANE) {
+			        vector<double> distances = road.findClosestDistanceForwardAndBehind(self_vehicle, self_vehicle.get_lane());
+			        double distanceForward = distances[1];
+			        if(distanceForward <= 30) {
+				        target_speed = start_speed * 0.8;
+			        }
+		        }
+		        else if(next_state == State::CHANGE_LEFT) {
+			        target_d = get_lane_d(self_vehicle.get_lane() - 1);
+		        }
+		        else if(next_state == State::CHANGE_RIGHT) {
+			        target_d = get_lane_d(self_vehicle.get_lane() + 1);
+		        }
+		        // right lane adjustment
+		        if(target_d >= 10.0) {
+			        target_d -= 0.2;
+		        }
 
-						generate_new_points = true;
-					}
+		        generate_new_points = true;
+	        }
 
-					if(generate_new_points) {
-						double target_s = self_vehicle.prev_s[0] + ((start_speed + target_speed)/2 * HORIZON);
-						vector<double> end_s = {target_s, target_speed, 0.0};
-						vector<double> end_d = {target_d, 0.0, 0.0};
+	        if(generate_new_points) {
+		        double target_s = self_vehicle.prev_s[0] + ((start_speed + target_speed)/2 * HORIZON);
+		        vector<double> end_s = {target_s, target_speed, 0.0};
+		        vector<double> end_d = {target_d, 0.0, 0.0};
 
-						self_vehicle.setPrev(end_s, end_d);
+	          self_vehicle.setPrev(end_s, end_d);
 
-						vector<double> jmt_s = JMT(start_s, end_s, HORIZON);
-						vector<double> jmt_d = JMT(start_d, end_d, HORIZON);
+		        vector<double> jmt_s = JMT(start_s, end_s, HORIZON);
+		        vector<double> jmt_d = JMT(start_d, end_d, HORIZON);
 
-						generateNewPoints(map, jmt_s, jmt_d, next_x_vals, next_y_vals);
-						cout << "StartS: " << start_s[0] << "StartD: " << start_d[0] << "; TargetS: " << target_s << " TargetD: " << target_d << endl;
-						
-					}
+		        generateNewPoints(map, jmt_s, jmt_d, next_x_vals, next_y_vals);
+		        cout << "StartS: " << start_s[0] << "StartD: " << start_d[0] << "; TargetS: " << target_s << " TargetD: " << target_d << endl;
+	        }
 
-					json msgJson;
-					msgJson["next_x"] = next_x_vals;
-					msgJson["next_y"] = next_y_vals;
+	        json msgJson;
+	        msgJson["next_x"] = next_x_vals;
+	        msgJson["next_y"] = next_y_vals;
 
-					auto msg = "42[\"control\","+ msgJson.dump()+"]";
+	        auto msg = "42[\"control\","+ msgJson.dump()+"]";
 
-					//this_thread::sleep_for(chrono::milliseconds(1000));
-					ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+	        //this_thread::sleep_for(chrono::milliseconds(1000));
+	        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
       } else {
         // Manual driving
